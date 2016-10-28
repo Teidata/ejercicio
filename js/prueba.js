8006c3b23db9e6dc3b4be24effe71270f15a92bd
Teidata <spa@teidata.com>
2016-10-28 20:25:05 +0100

    Add files via upload

---
+++ js/prueba.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+// PhotoSphere ------------------
+	var PSV = PhotoSphereViewer({
+	  panorama:  'http://www.sergioacosta.es/prueba/data/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+	  container: 'photosphere',
+	  loading_img: 'http://www.sergioacosta.es/prueba/data/giphy.gif',
+	});
+
+// CookieBar ------------------
+// Opción 1: Inicializar cookieBar (genérico, en inglés)
+$(document).ready(function() {
+  $.cookieBar();
+});
+
+
+});
